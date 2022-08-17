a7de1f7255e93e8a36367fd38bc5448dd1ee9824
Christopher Bradford <cbradford@momnt.com>
2022-08-17 15:44:53 +0000

    Update to test dynamic config for vue app. Should only run vue workflow

--- demo_vue/src/App.vue
+++ demo_vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Update to test dynamic config for vue app"/>
   </div>
 </template>
 
